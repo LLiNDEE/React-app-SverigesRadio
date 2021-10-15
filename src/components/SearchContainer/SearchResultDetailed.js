3b0922ad6550283bf4450f8fa76f9fcac283417c
LLiNDEE <africanbonesusdf@gmail.com>
2021-10-15 22:25:37 +0200

    Added icons to toggle between list and detailed-list instead of using text

--- src/components/SearchContainer/SearchResultDetailed.js
+++ src/components/SearchContainer/SearchResultDetailed.js
@@ -5,11 +5,13 @@ import EpisodeContext from '../EpisodeContext';
 // Imported icons
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ViewListIcon from '@mui/icons-material/ViewList';
+import ViewModuleIcon from '@mui/icons-material/ViewModule';
 
 // import Episode from './Episode';
 import EpisodeDetailed from './EpisodeDetailed';
 
-export default function SearchResultDetailed({ setView }){
+export default function SearchResultDetailed({ setView, view }){
 
     const {searchTerm, episodes, totalHits, totalPages, currentPage, prevPageHandler, nextPageHandler, getEpisodesByPage} = useContext(EpisodeContext);
 
@@ -27,6 +29,15 @@ export default function SearchResultDetailed({ setView }){
         setView("");
     }
 
+    function viewListHandler(){
+        setView("list");
+    }
+
+    function viewDetailedListHandler(){
+        setView("detailed");
+    }
+
+
     return(
         <div className="searchResult_detailed">
         <p>Sök resultat för {searchTerm}</p>
@@ -35,13 +46,18 @@ export default function SearchResultDetailed({ setView }){
             <input className="pageNum_btn" type="submit" value="Gå"/>
 
         </form>
-        
+        <div className="searchResult_btnICON_div">
+            <div className="icon">
+                <ViewListIcon sx={{ fontSize: 32 }} onClick={viewListHandler} className={(view == 'list') ? "viewBTN active" : "viewBTN" } />
+                <ViewModuleIcon sx={{ fontSize: 32 }} onClick={viewDetailedListHandler} className={(view == 'detailed')? "viewBTN2 active" : "viewBTN2"}/>
+            </div>  
+        </div>
         <div className="searchResult_pagesBTN">
             <ArrowBackIosIcon onClick={prevPageHandler} className="arrow_icon_left"/>
             <p>Sida {currentPage} av {totalPages}</p>
             <ArrowForwardIosIcon onClick={nextPageHandler} className="arrow_icon_right"/>
         </div>
-        <p className="searchResultDetailed_showNormalList" onClick={goBackHandler}>Visa mindre detaljerad lista</p>
+        <p className="searchResultDetailed_showNormalList">{totalHits} träffar</p>
         <div className="episode_container">
         {episodes.map(episode=>(
                         <EpisodeDetailed key={episode.id} episode={episode}/>
